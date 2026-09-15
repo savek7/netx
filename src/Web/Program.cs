d30aaee0f166216cfedd2d49e1dcecf273267752
przemek <przemek.szpara@gmail.com>
2026-09-15 21:33:56 +0200

    Add Serilog logging

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,14 +1,14 @@
-using Microsoft.AspNetCore.Http.HttpResults;
-using System.Text.Json.Serialization;
+using Tools.Logging;
 
 var builder = WebApplication.CreateSlimBuilder(args);
-
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
 });
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddLogging(builder.Logging, builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/tools/Tools.Logging/Installer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Tools.Logging;
+
+public static class Installer
+{
+    extension(IServiceCollection serviceCollection)
+    {
+        public IServiceCollection AddLogging(ILoggingBuilder builder, IConfiguration configuration)
+        {
+            ArgumentNullException.ThrowIfNull(configuration);
+
+            builder.ClearProviders();
+            serviceCollection.AddSerilog(configureLogger =>
+            {
+                configureLogger.MinimumLevel.Is(GetMinimumLevel(configuration));
+                configureLogger.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+
+                configureLogger
+                    .WriteTo.Console()
+                    .WriteTo.Conditional(le => bool.Parse(configuration["SerilogLogging:File:Enabled"]!) && !string.IsNullOrEmpty(configuration["SerilogLogging:File:Path"]),
+                        configureSink => configureSink.Async(t => t.File(configuration["SerilogLogging:File:Path"]!, rollingInterval: RollingInterval.Day)));
+            });
+
+            return serviceCollection;
+        }
+    }
+
+    static LogEventLevel GetMinimumLevel(IConfiguration configuration) => configuration["Logging:Serilog:LogLevel:Default"] switch
+    {
+        "Trace" => LogEventLevel.Verbose,
+        "Debug" => LogEventLevel.Debug,
+        "Information" => LogEventLevel.Information,
+        "Warning" => LogEventLevel.Warning,
+        "Error" => LogEventLevel.Error,
+        "Critical" => LogEventLevel.Fatal,
+
+        _ => LogEventLevel.Information
+    };
+}
+
